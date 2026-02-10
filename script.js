95534f894ed146fff73ad97e97d674229a8017b9
iloveyoulucas196-debug <iloveyoulucas196@gmail.com>
2026-02-10 16:22:49 -0300

    script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const hoy = new Date();
 const dia = hoy.getDate();
 const mes = hoy.getMonth(); // enero = 0, febrero = 1
 
-if (true) {
+if (dia === 14 && mes === 1) {
     document.getElementById("titulo").textContent =
         `Feliz San Valentín mi amor 💘`;
 
@@ -43,4 +43,5 @@ if (true) {
 
     document.getElementById("boton").style.display = "inline-block";
 }
+
 
